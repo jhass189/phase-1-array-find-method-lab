44297ed6ac5ef1779c8ed1c828f561b8a1750988
James Hassett <jhassett189@gmail.com>
2022-06-15 21:25:21 -0400

    commit tree 6037857df1cb85c54958030036a76d6613856ffa

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(win) {
+    let result = win.find((obj) => obj.result == "W")
+    console.log(result)
+    if (result) {
+        return result.year
+
+    }
+}
